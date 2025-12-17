c75cd46c8c1bc2c5eed1e382ebaa3f0bc7710c7c
Jacobs David <jacobsdavid.dr@gmail.com>
2025-12-17 11:31:24 +0100

    Initial commit

---
+++ app/_layout.tsx
@@ -0,0 +1,38 @@
+import * as SplashScreen from "expo-splash-screen";
+import { Stack } from "expo-router";
+import "react-native-reanimated";
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      setTimeout(() => {
+        SplashScreen.hideAsync();
+      }, 10000); // 3 seconds
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "../global.css";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-blue-800">Home</Text>
+    </SafeAreaView>
+  );
+};
+export default Home;
